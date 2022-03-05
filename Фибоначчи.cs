59f6275c86a60f84b857d6d8f9165cb57dafc839
Gsgshn <100374838+Gsgshn@users.noreply.github.com>
2022-03-05 23:02:10 +0300

    Фибоначчи made in users!!!!!

---
+++ Фибоначчи.cs
@@ -0,0 +1,20 @@
+﻿using System;
+Console.Write("Ввидете количество чисел в ряду Фибоначчи: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Числа Фибоначчи:");
+int[] array = new int[n];
+array[0] = 0;
+array[1] = 1;
+Console.Write($"{array[0]}, {array[1]} ");
+for (int i = 2; i < n; i++)
+{
+    int first = array[i - 1];
+    int second=array[i - 2];
+    array[i] = first + second;
+
+    Console.Write($", {array[i]} "); 
+    first = second;
+    second = array[i];
+}
+
