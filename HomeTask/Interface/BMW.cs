421aff52ca5fa816c50c8955a795ea9f0956d2ca
Сергей Лукьяненко <100523144+OldarSergey@users.noreply.github.com>
2022-03-25 21:09:33 +0300

    Delete HomeTask directory

--- HomeTask/Interface/BMW.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace HomeTask
-{
-    public class BMW : Car
-    {
-        
-    }
-}
--- HomeTask/Interface/LadaX4.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace HomeTask
-{
-    public class LadaX4 : Car
-    {
-        
-    }
-}
