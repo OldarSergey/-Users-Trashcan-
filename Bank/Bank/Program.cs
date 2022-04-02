e0318a88f98f6b3b91ea99612561f26103a8c6da
Gsgshn <100374838+Gsgshn@users.noreply.github.com>
2022-04-02 11:38:31 +0300

    Add files via upload

--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -8,14 +8,18 @@ namespace Proect
     {
         public static void Main(string[] args)
         {
+            Random random = new Random();
             Bank bank = new Bank("VTB", 900);
             Bank bank1 = new Bank("Tinkoff", 841);
             Client client = new Client("Сергей", "Лукьяненко");
+            Client client2 = new Client("Дмитрий", "Лазарев");
             Check adress = new Check("пупкина 9");
             Console.Write("Введите сумму операции: ");
             Check sum = new Check(Convert.ToInt32(Console.ReadLine()));
             BankCard bankCard = new(8);
-            bankCard.CardNumber = 879879879;
+            BankCard bankCard1 = new(8);
+            bankCard1.CardNumber = random.Next(1,999999999);
+            bankCard.CardNumber = random.Next(1, 999999999);
             int balance = 23423;
             BankСard Balance = new BankСard(balance);
             Console.Write($"Возможные типы операций:\n 1 - Пополнение\n 2 - Перевод \n 3 - Снятие наличных\nВыберите тип операции: ");
@@ -27,7 +31,7 @@ namespace Proect
                     balance = balance + sum.Sum;
                     Console.WriteLine($"Наименование банка: {bank1.Name}\n{adress.Adress}\nТелефон службы помощи клиентам: {bank1.HotLine}" +
                     $"\n+7(903)435-59-57\nТип операции:{TypeOperation.Пополнение}" +
-                    $"\n{DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year} {DateTime.Now.TimeOfDay} \n{client.FirstName} {client.LastName}\n" +
+                    $"\n{DateTime.Now} \n{client.FirstName} {client.LastName}\n" +
                     $"Сумма операции:{sum.Sum}\nНомер карты: {bankCard.CardNumber}\nВаш баланс: {balance}");
                     break;
                 case 2:
@@ -36,8 +40,8 @@ namespace Proect
                     {
                         Console.WriteLine($"Наименование банка: {bank1.Name}\n{adress.Adress}\nТелефон службы помощи клиентам: {bank1.HotLine}" +
                         $"\n+7(903)435-59-57\nТип операции:{TypeOperation.Перевод}" +
-                        $"\n{DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year} {DateTime.Now.TimeOfDay} \n{client.FirstName} {client.LastName}\n" +
-                        $"Сумма операции:{sum.Sum}\nНомер карты: {bankCard.CardNumber}\nВаш баланс: {balance}");
+                        $"\n{DateTime.Now} \nОт: {client.FirstName} {client.LastName}\nКому: {client2.FirstName} {client2.LastName} {bankCard1.CardNumber} " +
+                        $"\nСумма операции:{sum.Sum}\nНомер карты: {bankCard.CardNumber}\nВаш баланс: {balance}");
                     }
                     else Console.WriteLine("Операция невозможна. Пополните баланс.");
                     break;
@@ -47,7 +51,7 @@ namespace Proect
                     {
                         Console.WriteLine($"Наименование банка: {bank1.Name}\n{adress.Adress}\nТелефон службы помощи клиентам: {bank1.HotLine}" +
                         $"\n+7(903)435-59-57\nТип операции:{TypeOperation.Перевод}" +
-                        $"\n{DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year} {DateTime.Now.TimeOfDay} \n{client.FirstName} {client.LastName}\n" +
+                        $"\n{DateTime.Now} \n{client.FirstName} {client.LastName}\n" +
                         $"Сумма операции:{sum.Sum}\nНомер карты: {bankCard.CardNumber}\nВаш баланс: {balance}");
 
                     }
