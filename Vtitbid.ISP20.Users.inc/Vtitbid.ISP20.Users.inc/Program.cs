66907d068afc6728c2a875c801c9d10ead211eae
Gsgshn <100374838+Gsgshn@users.noreply.github.com>
2022-10-22 00:17:34 +0300

    Add files via upload

---
+++ Vtitbid.ISP20.Users.inc/Vtitbid.ISP20.Users.inc/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Vtitbid.ISP20.Users.inc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Zodiac kirill = new Zodiac();
+            Console.WriteLine(Zodiac.GetZodiacsArray()); 
+            
+            System.Console.WriteLine(kirill);
+            
+            
+        }
+    }
+}
---
+++ Vtitbid.ISP20.Users.inc/Vtitbid.ISP20.Users.inc/ZodiacSign.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vtitbid.ISP20.Users.inc
+{
+     public enum ZodiacSign
+    {
+        Aries ,
+        Taurus ,
+        Gemini ,
+        Cancer ,
+        Leo ,
+        Virgo ,
+        Libra ,
+        Scorpio ,
+        Sagittarius ,
+        Capricorn ,
+        Aquarius ,
+        Pisces 
+    }
+}
