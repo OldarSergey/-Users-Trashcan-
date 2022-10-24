83148a688c09c71b8df18be5efcaf8db675ed7c1
Gsgshn <100374838+Gsgshn@users.noreply.github.com>
2022-10-24 23:28:31 +0300

    Add files via upload

---
+++ Vtitbid.ISP20.Users.inc.Note/Vtitbid.ISP20.Users.inc.Note/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Vtitbid.ISP20.Users.inc.Note
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите количество людей:");
+            if (int.TryParse(Console.ReadLine(), out int value))
+            {
+                Note[] array = Note.GetNoteArray(value);
+                Console.WriteLine();
+                foreach (var note in array)
+                {
+                    Console.WriteLine(note);
+                    Console.WriteLine();
+
+
+                }
+                Note.SearchNote(array);
+            }
+            else
+            {
+                Console.WriteLine("Количесвто людей не опредлено ");
+            }
+
+
+
+        }
+    }
+}
