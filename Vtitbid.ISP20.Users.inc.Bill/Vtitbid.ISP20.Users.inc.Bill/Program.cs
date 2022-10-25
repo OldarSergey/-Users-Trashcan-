e183c6c70d72c5b67988467b4e0aaf2178a59375
Gsgshn <100374838+Gsgshn@users.noreply.github.com>
2022-10-25 22:35:16 +0300

    Add files via upload

---
+++ Vtitbid.ISP20.Users.inc.Bill/Vtitbid.ISP20.Users.inc.Bill/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Vtitbid.ISP20.Users.inc.Bill
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите количество операций:");
+            if (int.TryParse(Console.ReadLine(), out int value))
+            {
+                Bill[] array = Bill.GetBillArray(value);
+                
+                Console.WriteLine();
+                foreach (var route in array)
+                {
+                    Console.WriteLine(route);
+                    Console.WriteLine();
+                }
+
+                Bill.SearchBill(array);
+                
+            }
+            else
+            {
+                Console.WriteLine("Количесвто операций не опредлено ");
+            }
+
+
+
+
+        }
+    }
+}
