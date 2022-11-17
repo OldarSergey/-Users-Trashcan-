05491a964bc0fee2a5c21251c8e74d1fb820c909
Сергей Лукьяненко <100523144+OldarSergey@users.noreply.github.com>
2022-11-18 00:47:06 +0300

    Add files via upload

---
+++ Vtitbid.ISP20.LukyanenkoLazarev.TRPO2/Game.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vtitbid.ISP20.LukyanenkoLazarev.TRPO1
+{
+    public static class Game
+    {
+        static bool repeat = true;
+        static private string[] game = new string[] { "камень","ножницы", "бумага" };
+
+        static Random rand = new Random();
+        public static void StoneScissorsPaper()
+        {
+            while (repeat)
+            {
+                string random = game[rand.Next(game.Length)];
+                Console.Write("Камень, ножницы или бумага? : ");
+                string input = Console.ReadLine();
+
+                switch (input.ToLower())
+                {
+                    case "камень":
+                        Console.WriteLine(Stone(random, input));
+                        break;
+                    case "ножницы":
+                        
+                        Console.WriteLine(Scissors(random, input));
+                        break;
+                    case "бумага":
+                        Console.WriteLine(Paper(random, input));
+                        break;
+                    default:
+                        Console.WriteLine("ты по-моему перепутал");
+                        break;
+                }
+                Console.Write("1 - Продолжить\n2 - выйти : ");
+                string choice = Console.ReadLine();
+                    if (choice == "2") { repeat = false; }
+                else Console.WriteLine("играем дальше");
+                    
+            }
+            
+        }
+        public static string Stone(string random , string input)
+        {
+            string output;
+            if (random == "ножницы")
+            {
+
+               output = $"Ты выиграл. {input} мощнее, чем {random}";   
+            }
+            else if (random == "бумага")
+            {
+                output = $"Я выиграл. {input} слабее, чем {random}";
+            }
+            else output = $"{input} = {random}. Мы оба хороши";
+
+            return output;
+        }
+        public static string Scissors(string random, string input)
+        {
+            string output;
+            if (random == "бумага")
+            {
+
+                output = $"Ты выиграл. {input} мощнее, чем {random}";
+            }
+            else if (random == "камень")
+            {
+                output = $"Я выиграл. {input} слабее, чем {random}";
+            }
+            else output = $"{input} = {random}. Мы оба хороши";
+
+            return output;
+        }
+        public static string Paper(string random, string input)
+        {
+            string output;
+            if (random == "камень")
+            {
+
+                output = $"Ты выиграл. {input} мощнее, чем {random}";
+            }
+            else if (random == "ножницы")
+            {
+                output = $"Я выиграл. {input} слабее, чем {random}";
+            }
+            else output = $"{input} = {random}. Мы оба хороши";
+
+            return output;
+        }
+
+
+    }
+}
---
+++ Vtitbid.ISP20.LukyanenkoLazarev.TRPO2/Program.cs
@@ -0,0 +1,13 @@
+﻿using Vtitbid.ISP20.LukyanenkoLazarev.TRPO1;
+
+namespace Vtitbid.ISP20.LukyanenkoLazarev.TRPO
+{
+    public sealed class Program 
+    {
+        public static void Main (string[] args)
+        {
+            Game.StoneScissorsPaper();
+        }
+    }
+
+}
